346694bbcf10b0258993729e7ed49efb837f3898
Gabe <gabrielestival33@gmail.com>
2022-11-28 11:26:40 -0300

    1.0

---
+++ src/components/Resume/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ResumeItem from "../ResumeItem";
+import * as C from "./styles";
+import {
+  FaRegArrowAltCircleUp,
+  FaRegArrowAltCircleDown,
+  FaDollarSign,
+} from "react-icons/fa";
+
+const Resume = ({ income, expense, total }) => {
+  return (
+    <C.Container>
+      <ResumeItem
+        title="Incomes"
+        Icon={FaRegArrowAltCircleUp}
+        value={income}
+      />
+      <ResumeItem
+        title="Bills"
+        Icon={FaRegArrowAltCircleDown}
+        value={expense}
+      />
+      <ResumeItem title="Total" Icon={FaDollarSign} value={total} />
+    </C.Container>
+  );
+};
+
+export default Resume;
